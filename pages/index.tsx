5d9ea694af2ae2e5b0932934cc4fc9e4d085fdbd
liupei17 <liupei17@jd.com>
2024-10-31 23:06:28 +0800

    feat: 格式化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,5 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
-import Hero from "@/pages/home/hero"
-import Image from 'next/image'
+import Hero from "@/pages/home/hero";
 
 export default function IndexPage() {
   return (
